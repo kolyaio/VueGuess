a31a3d2df2072e2c4d2b34d0078c56d82495fe09
Niko <nick2ilk@gmail.com>
2016-09-22 23:16:14 +0300

    many changes

--- js/neoLogic.js
+++ js/neoLogic.js
@@ -3,76 +3,80 @@ var vm = new Vue({
     data: {
       message: "",
       counter: 0,
-      clickCount: 0,
       MAX_RAND: 10,
       MIN_RAND: 0,
       showTable: false,
-      numbers: {
-        randoms:[
-          { value: 0 },
-          { value: 0 },
-          { value: 0 }
-        ],
-        guesses:[
-          { value: 0 },
-          { value: 0 },
-          { value: 0 }
-        ]
-      },
+      randoms: [0,0,0],
+      guesses: [0,0,0],
     },
     methods: {
-      randomize: function(event) {
+      getRandomNumber: function(event) {
         return Math.floor((Math.random() * this.MAX_RAND)+this.MIN_RAND);
       },
       guess: function(event) {
         let guessForm = document.querySelector('#guessForm')
         if(guessForm.checkValidity()) {
           event.preventDefault();
-          let _this = this;
-          this.showTable = true;
-          this.numbers.randoms.forEach(function(element) {
-            element.value = _this.randomize();
-          })
-          for(let index in [...Array(3)]) {
-            // if(index == 0) {
-            //   console.log('///////////////////////////////////////////////')
-            // }
-            if(this.numbers.randoms[index].value == this.numbers.guesses[index].value) {
-              _this.counter = _this.counter + 1;
-            }
-            // console.log("randoms: " + this.numbers.randoms[index].value)
-            // console.log("gusses: " + this.numbers.guesses[index].value)
-            // console.log("counter: " + _this.counter);
-            // console.log('------------------------------------------------')
-          }
-          _this.counter = 0;
+          this.counter = 0;
 
-          if (this.counter == 3) {
-            this.message = "Well done you guessed all the numbers!! You lucky bastard!!";
+          for(let i in this.randoms) {
+            this.randoms.$set(i, this.getRandomNumber());
           }
-          else if (this.counter == 2) {
-            this.message = "Well done you guessed 2 numbers!! ";
+
+          this.showTable = true;
+
+          for(let i in [...Array(3)]) {
+            if(i == 0) {
+               console.log('///////////////////////////////////////////////')
+            }
+
+
+            if(this.randoms[i] == this.guesses[i]) {
+              this.counter++;
+              console.log(this.counter);
+              console.log("randoms: " + this.randoms[i])
+              console.log("gusses:  " + this.guesses[i])
+              console.log('------------------------------------------------')
+            }
           }
-          else if (this.counter == 1 ) {
-            this.message = "You guessed one at least!";
+
+
+          console.table("RANDOMS: " + this.randoms)
+          console.table("GUESSES: " + this.guesses)
+
+
+
+          switch (this.counter) {
+            case 3:
+                this.message = "Well done you guessed all the numbers!! You lucky bastard!!";
+              break;
+            case 2:
+              this.message = "Well done you guessed 2 numbers!! ";
+              break;
+            case 1:
+                this.message = "You guessed one at least!";
+                break;
+            case 0:
+            default:
+                this.message = "You unlucky, You didn't guessed even one.";
+                break;
           }
-          else if(this.counter == 0){
-            this.message = "You unlucky, You didn't guessed even one.";
-          }
-          if(this.clickCount > 0) {
-            this.message = ""
-          }
-        }
+
+          console.log("MESSAGE: " + this.message)
+          console.log('------------------------------------------------')
+        } // checkValidity closing
       },
       reset: function(evemt) {
         event.preventDefault();
         this.showTable = false;
-        this.numbers.randoms.forEach(function(element) {
-          element.value = 0;
-        })
-        this.numbers.guesses.forEach(function(element) {
-          element.value = 0;
-        })
+
+        for(let i in this.randoms) {
+          this.randoms.$set(i, 0);
+        }
+
+        for(let i in this.guesses) {
+          this.guesses.$set(i, 0);
+        }
         this.message = "";
       },
     }
